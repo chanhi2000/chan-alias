7f819ef4c0af6eb6a7352499cf506a3e46fe050e
Chan Hee Lee <chanhi2000@gmail.com>
2025-02-13 11:11:13 +0900

    [mod] 갱신 2025-02-21

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,27 @@
 <#
 .SYNOPSIS
-	Chan Powershell Profile
+    Chan Powershell Profile
 .DESCRIPTION
-	Aliases 및 중요 파일경로 관리
+    Aliases 및 중요 파일경로 관리
 .LINK
-	https://github.com/chanhi2000/chan-alias
+    https://github.com/chanhi2000/chan-alias
 .NOTES
-	Author: chanhi2000 | License: CC0
+    Author: chanhi2000 | License: CC0
 #>
 
 # 사용자 설정 경로
 $env:PATH_ALAIS_HOME = $profile
-$env:PATH_ONNARA_ANDROID = "D:\development\android\OnnaraMobileAndroid"
+
+$env:PATH_PUB = "C:\Users\Public\Documents"
+$env:PATH_DEV = "C:\development"
+$env:PATH_DEV_ITITCLOUD = "$env:PATH_DEV/ititcloud"
+$env:PATH_DEV_RUTIL_VM = "$env:PATH_DEV_ITITCLOUD/rutil-vm"
+$env:DOCKER_TAG_RUTIL_VM = "ititcloud/rutil-vm"
+$env:DOCKER_TAG_RUTIL_VM_API = "ititcloud/rutil-vm-api"
+$env:DOCKER_TAG_RUVIL_VM_API_VERSION = "0.2.0-beta2"
+$env:DOCKER_TAG_RUTIL_VM_CURRENT = "${env:DOCKER_TAG_RUTIL_VM}:${env:DOCKER_TAG_RUVIL_VM_API_VERSION}"
+$env:DOCKER_TAG_RUTIL_VM_API_CURRENT = "${env:DOCKER_TAG_RUTIL_VM_API}:${env:DOCKER_TAG_RUVIL_VM_API_VERSION}"
+
 $env:GIT_LOG_FORMAT_DEFAULT = "%Cred%h%Creset %C(yellow)%d%Crest %s %Cgreen(%cr) %C(bold blue) %an %Creset"
 
 Write-Host @"
@@ -19,13 +29,18 @@ Write-Host @"
                ENVIRONMENT VARIABLES
 ===================================================
 
- [PATH_ALAIS_HOME]: $profile
- [PATH_ONNARA_ANDROID]: $env:PATH_ONNARA_ANDROID
- [GIT_LOG_FORMAT_DEFAULT]: $env:GIT_LOG_FORMAT_DEFAULT
+[PATH_ALAIS_HOME]: $profile
+[PATH_DEV]: $env:PATH_DEV
+[DOCKER_TAG_RUTIL_VM_API]: $env:DOCKER_TAG_RUTIL_VM_API
+[DOCKER_TAG_RUTIL_VM_API_CURRENT]: $env:DOCKER_TAG_RUTIL_VM_API_CURRENT
+
 
 ===================================================
                      Aliases
-===================================================
+=================================================== 
+
+[cdd] - go to development directory
+[open] - open directory in Windows Explorer
 
 [g] - git
 [gs] - git status
@@ -37,13 +52,20 @@ Write-Host @"
 [glg] - git log  ...
 [gp] - git push  ...
 [gpl] - git pull  ...
- 
- 
-[cdWHA] - change directory to OnnaraMobileAndroid ...
-[codeHWAW1] - open VSCode for `onnara01\src\main\assets`
-[codeHWAW2] - open VSCode for `onnara02\src\main\assets` 
 
- 
+[cddc] - change directory to $env:PATH_DEV\chanhi200 ....
+[cddi] - change directory to $env:PATH_DEV\ititcloud ...
+
+[m3u8Get <SOURCE> <OUTPUT>] - convert m3u8 to media output (*.avi, *.mp4, ...)
+
+[chcl] - choco list
+[chci] - choco install -y <PACKAGE_NAME>
+[chcu] - choco upgrade -y <PACKAGE_NAME>
+
+[scl] - scoop list
+[sci] - scoop install -y <PACKAGE_NAME>
+[scu] - scoop update <PACKAGE_NAME>
+
 [scrcpyDefault] - run scrcpy with default settings
 [scrcpyRec] - run scrcpy showing touches
 [killTestbed] - kill testbed agent using adb
@@ -52,10 +74,34 @@ Write-Host @"
 [alias] - alias configure
 "@
 
+# Dracula readline configuration. Requires version 2.0, if you have 1.2 convert to `Set-PSReadlineOption -TokenType`
+Set-PSReadlineOption -Color @{
+    "Command" = [ConsoleColor]::Green
+    "Parameter" = [ConsoleColor]::Gray
+    "Operator" = [ConsoleColor]::Magenta
+    "Variable" = [ConsoleColor]::White
+    "String" = [ConsoleColor]::Yellow
+    "Number" = [ConsoleColor]::Blue
+    "Type" = [ConsoleColor]::Cyan
+    "Comment" = [ConsoleColor]::DarkCyan
+}
+# Dracula Prompt Configuration
+Import-Module posh-git
+$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
+$GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
+$GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
+$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
+# Dracula Git Status Configuration
+$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
+
+
 # git
 function checkGit() {
     $Null = (git --version)
-	if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
+    if ($lastExitCode -ne "0") { throw "Can't execute 'git' - make sure Git is installed and available" }
 }
 function g() { git }
 function gs() { git status }
@@ -99,20 +145,75 @@ function gpl()
 
 
 # 개발환경 구성
-function cdWHA() { cd $env:PATH_ONNARA_ANDROID }
-function codeHWAW1() { code "$env:PATH_ONNARA_ANDROID\onnara01\src\main\assets" }
-function codeHWAW2() { code "$env:PATH_ONNARA_ANDROID\onnara02\src\main\assets" }
-
-
+function cdp() { cd $env:PATH_PUB }
+function cdd() { cd $env:PATH_DEV }
+function open() { 
+    param(
+        [string] $location="."
+    )
+    explorer $param
+}
+function cddc() { cd "$env:PATH_DEV\chanhi2000"; }
+function cddi() { cd "$env:PATH_DEV_ITITCLOUD"; }
+function m3u8Get() {
+    param(
+        [string] $source=".",
+        [string] $output="."
+    )
+    ffmpeg -protocol_whitelist https,tls,tcp -allowed_extensions ALL -i $source -bsf:a aac_adtstoasc -c copy $output
+}
+#
+# choco 관련 aliases
+#
+function chcl() { choco list }
+function chci() { 
+    param(
+        [string] $p=""
+    )
+    choco install -y $p
+}
+function chcu {
+    param(
+        [string] $p=""
+    )
+    choco upgrade -y $p
+}
+#
+# scoop 관련 aliases
+#
+function scl() { scoop list }
+function sci() {
+    param(
+        [string] $p=""
+    )
+    scoop install -y $p
+}
+function scu {
+    param(
+        [string] $p=""
+    )
+    scoop update $p
+}
 # ADB 및 안드로이드 관련
 function scrcpyDefault() { scrcpy -m 1024 --always-on-top }
 function scrcpyRec() { scrcpy -m 1024 --always-on-top --show-touches }
 function killTestbed() { adb shell am force-stop kr.go.mobile.testbed.iff }
+
+# RutilVM 프로젝트 관련
+function di() { docker images }
+function drmib { docker rmi $env:DOCKER_TAG_RUTIL_VM_API_CURRENT }
+function buildDkb { docker build -t $env:DOCKER_TAG_RUTIL_VM_API_CURRENT $env:PATH_DEV_RUTIL_VM\back }
+function saveDkb { docker save -o api.tar $env:DOCKER_TAG_RUTIL_VM_API_CURRENT }
+
+function drmif { docker rmi $env:DOCKER_TAG_RUTIL_VM_CURRENT }
+function buildDkf { docker build -t $env:DOCKER_TAG_RUTIL_VM_CURRENT $env:PATH_DEV_RUTIL_VM\front }
+function saveDkf { docker save -o web.tar $env:DOCKER_TAG_RUTIL_VM_CURRENT }
 
 function alias() { notepad $profile }
 
 # 후처리
 $env:PATH += ";$env:UserProfile\scoop\apps\oh-my-posh\current\bin"
 oh-my-posh --init --shell pwsh --config "$env:UserProfile\scoop\apps\oh-my-posh\current\themes\agnoster.omp.json" | Invoke-Expression
+fnm env --use-on-cd | Out-String | Invoke-Expression
 Import-Module Terminal-Icons
-# neofetch
+fastfetch
--- alias.cmd
+++ alias.cmd
@@ -6,16 +6,24 @@
 :: - 레지스트리에서 \HKEY_CURRENT_USER\SOFTWARE\Microsoft\Command Processor경로 이동
 :: - 키 생성 (문자열)
 ::   - 이름: AutoRun
-::   - 값: alias.cmd를 저장한 절대경로 (이 경로가 PATH_ALAIS_HOME값과 같아야 함)
+::   - 값: alias.cmd를 저장한 절대경로 (이 경로가 PATH_ALIAS_HOME값과 같아야 함)
 ::
 :: REG ADD "HKCU\SOFTWARE\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d D:\alias.cmd
 ::
 
 :: 사용자 설정 경로 (필수)
-REM SET PATh_ALAIS_HOME=%USERPROFILE%
+SET PATH_ALIAS_HOME=%USERPROFILE%
 SET ALIAS_FNAME=alias.cmd
-SET PATH_ALAIS_HOME=D:\%ALIAS_FNAME%
-SET PATH_ONNARA_ANDROID=D:\development\android\OnnaraMobileAndroid
+
+SET PATH_PUB=C:\Users\Public\Documents
+SET PATH_DEV=C:\development
+SET PATH_DEV_ITITCLOUD=%PATH_DEV%\ititcloud
+SET PATH_DEV_RUTIL_VM=%PATH_DEV_ITITCLOUD%\rutil-vm
+SET DOCKER_TAG_RUTIL_VM=ititcloud/rutil-vm
+SET DOCKER_TAG_RUTIL_VM_API=ititcloud/rutil-vm-api
+SET DOCKER_TAG_RUVIL_VM_API_VERSION=0.2.0-beta2
+SET DOCKER_TAG_RUTIL_VM_CURRENT=%DOCKER_TAG_RUTIL_VM%:%DOCKER_TAG_RUVIL_VM_API_VERSION%
+SET DOCKER_TAG_RUTIL_VM_API_CURRENT=%DOCKER_TAG_RUTIL_VM_API%:%DOCKER_TAG_RUVIL_VM_API_VERSION%
 
 :: alias 사용법 설명
 ECHO.
@@ -24,17 +32,22 @@ ECHO ===================================================
 ECHO                ENVIRONMENT VARIABLES
 ECHO ===================================================
 ECHO.
-ECHO. [PATH_ALAIS_HOME]: %PATH_ALAIS_HOME%
-ECHO. [PATH_ONNARA_ANDROID]: %PATH_ONNARA_ANDROID%
+ECHO. [PATH_ALIAS_HOME]: %PATH_ALIAS_HOME%
+ECHO. [PATH_DEV]: %PATH_DEV%
+ECHO. [DOCKER_TAG_RUTIL_VM]: %DOCKER_TAG_RUTIL_VM%
+ECHO. [DOCKER_TAG_RUTIL_VM_API_CURRENT]: %DOCKER_TAG_RUTIL_VM_API_CURRENT%
 ECHO.
 ECHO ===================================================
 ECHO                      Aliases
 ECHO ===================================================
 ECHO.
+ECHO [cdd] - go to development directory
 ECHO [l] - list file(s) in the working directory
 ECHO [ls] - list file(s) in the working directory (simple)
+ECHO [rm] - delete file(s)
 ECHO [pwd] - print working directory
 ECHO [clear] - clear console screen
+ECHO [open] - open directory in Windows Explorer
 ECHO.
 ECHO [g] - git
 ECHO [gs] - git status
@@ -49,27 +62,31 @@ ECHO [gt] - git tag ...
 ECHO [gp] - git push  ...
 ECHO [gpl] - git pull  ...
 ECHO. 
-ECHO. 
-ECHO [cdWHA] - change directory to OnnaraMobileAndroid ...
-ECHO [codeWHAW1] - open VSCode for `onnara01\src\main\assets`
-ECHO [codeWHAW2] - open VSCode for `onnara02\src\main\assets` 
+ECHO [cddc] - change directory to `%PATH_DEV%\chanhi200` ....
+ECHO [cddi] - change directory to `%PATH_DEV%\ititcloud`...
 ECHO.
-ECHO. 
 ECHO [scrcpyDefault] - run scrcpy with default settings
 ECHO [scrcpyRec] - run scrcpy showing touches
 ECHO [killTestbed] - kill testbed agent using adb
-ECHO. 
-ECHO. 
+ECHO.
+ECHO [di] - docker images
+ECHO [drmi] - docker rmi rutil-vm-api 
+ECHO [buildDk] - build rutil-vm-api
+ECHO [saveDk] - save rutil-vm-api
+ECHO.
 ECHO [alias] - alias configure
-ECHO. 
 ECHO.
 
 
 :: Commands
-@DOSKEY l=dir /O $*
-@DOSKEY ls=dir /B $*
-@DOSKEY pwd=echo %%cd%%
-@DOSKEY clear=cls
+@DOSKEY cdp=CD %PATH_PUB%
+@DOSKEY cdd=CD %PATH_DEV%
+@DOSKEY l=DIR /O $*
+@DOSKEY ls=DIR /B $*
+@DOSKEY rm=DEL /S $*
+@DOSKEY pwd=ECHO %%cd%%
+@DOSKEY clear=CLS
+@DOSKEY open=EXPLORER $*
 
 :: git
 @DOSKEY g=git $*
@@ -87,15 +104,26 @@ ECHO.
 
 
 :: 개발환경 구성
-@DOSKEY cdWHA=%PATH_ONNARA_ANDROID:~0,1%: ^&^& CD %PATH_ONNARA_ANDROID% $*
-@DOSKEY codeWHAW1=code %PATH_ONNARA_ANDROID%\onnara01\src\main\assets $*
-@DOSKEY codeWHAW2=code %PATH_ONNARA_ANDROID%\onnara02\src\main\assets $*
-
+:: @DOSKEY cddc=CD %PATH_DEV%\chanhi2000 && EXPLORER . ^&^& $*
+:: @DOSKEY cddi=CD %PATH_DEV%\ititcloud && EXPLORER . ^&^& $*
+@DOSKEY cddc=CD %PATH_DEV%\chanhi2000
+@DOSKEY cddi=CD %PATH_DEV_ITITCLOUD%
+:: @DOSKEY cddi=code %DOCKER_TAG_RUTIL_VM%\onnara01\src\main\assets $*
+@DOSKEY m3u8Get=ffmpeg -protocol_whitelist https,tls,tcp -allowed_extensions
 
 :: ADB 및 안드로이드 관련
 @DOSKEY scrcpyDefault=scrcpy -m 1024 --always-on-top
 @DOSKEY scrcpyRec=scrcpy -m 1024 --always-on-top --show-touches
 @DOSKEY KillTestbed=adb shell am force-stop kr.go.mobile.testbed.iff
 
+:: RutilVM 프로젝트 관련
+@DOSKEY di=docker images $*
+@DOSKEY drmib=docker rmi %DOCKER_TAG_RUTIL_VM_API_CURRENT% $*
+@DOSKEY buildDkb=docker build -t %DOCKER_TAG_RUTIL_VM_API_CURRENT% %PATH_DEV_RUTIL_VM%\back
+@DOSKEY saveDkb=docker save -o api.tar %DOCKER_TAG_RUTIL_VM_API_CURRENT%
 
-@DOSKEY alias=notepad %PATH_ALAIS_HOME%
+@DOSKEY drmif=docker rmi %DOCKER_TAG_RUTIL_VM_CURRENT% $*
+@DOSKEY buildDkf=docker build -t %DOCKER_TAG_RUTIL_VM_CURRENT% %PATH_DEV_RUTIL_VM%\front
+@DOSKEY saveDkf=docker save -o web.tar %DOCKER_TAG_RUTIL_VM_CURRENT%
+
+@DOSKEY alias=notepad %PATH_ALIAS_HOME%
